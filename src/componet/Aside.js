116544766daa46145624b02d5e4cf726961f73fc
ruilinsun <garrolinlin@gmail.com>
2024-09-24 17:45:25 -0700

    basic components layout for homepage created

---
+++ src/componet/Aside.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Aside() {
+  return (
+    <aside className="aside">
+      <ul>
+        <li>Explore</li>
+        <li>Create</li>
+        <li>Personalize</li>
+        <li>Chat</li>
+        <li>Tasks</li>
+        <li>Help</li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Aside;
---
+++ src/componet/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main className="main-content">
+      <div className="content-card">Content 1</div>
+      <div className="content-card">Content 2</div>
+
+    </main>
+  );
+}
+
+export default Main;
